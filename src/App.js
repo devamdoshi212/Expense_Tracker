b2a2dec1234d6b8dafa41a7d58c3856fc3eb7ecf
Devam Doshi <devamdoshi212@gmail.com>
2023-07-21 12:42:45 +0530

    adding css to expense-item

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div className="App">
-      <h1>Hello there</h1>
       <ExpenseItem></ExpenseItem>
-      <h3>hello ppl</h3>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h1>Expense items!!</h1>;
+  return (
+    <div className="expense-item">
+      <div>21 July 2023</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$4598</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
