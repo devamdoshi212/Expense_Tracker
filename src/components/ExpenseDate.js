26a6f4d3a07d47422cb135509086001089472d24
Devam Doshi <devamdoshi212@gmail.com>
2023-07-21 14:24:34 +0530

    adding css to expense date

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,12 +1,13 @@
+import "./ExpenseDate.css";
 function ExpenseDate(data) {
   const month = data.date.toLocaleString("en-US", { month: "long" });
   const year = data.date.getFullYear();
   const day = data.date.toLocaleString("en-US", { day: "2-digit" });
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }
