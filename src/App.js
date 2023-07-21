1fd588914851edc090e029ebd5c145fb6ee353b7
Devam Doshi <devamdoshi212@gmail.com>
2023-07-21 11:18:27 +0530

    basic setup and first component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello there</h1>
+      <ExpenseItem></ExpenseItem>
+      <h3>hello ppl</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+function ExpenseItem() {
+  return <h1>Expense items!!</h1>;
+}
+
+export default ExpenseItem;
