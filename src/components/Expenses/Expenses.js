5bc7666e50443c7baaf27886433a382f00060982
Devam Doshi <devamdoshi212@gmail.com>
2023-07-27 22:46:56 +0530

    add filter option only ui

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
+import React, { useState } from "react";
 import "../Expenses/Expenses.css";
 import Card from "../UI/Card";
 import ExpenseItem from "../Expenses/ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
