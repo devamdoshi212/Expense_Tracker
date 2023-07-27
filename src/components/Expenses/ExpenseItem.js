60cc8158fc35716b8c490c2a55c30e84404048e2
Devam Doshi <devamdoshi212@gmail.com>
2023-07-27 12:15:58 +0530

    1st hook baisc , useState and update value,React render all components in first time if any variable value changed react dom dont render again that component . for again rendering we use useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,19 @@
+import React, { useState } from "react";
 import "../Expenses/ExpenseItem.css";
 import ExpenseDate from "../Expenses/ExpenseDate";
 import Card from "../UI/Card";
 function ExpenseItem(expenseitems) {
+  const [title, setTitle] = useState(expenseitems.title); //initial value pass in argument and in array [1,2] 1=initial state and if 2 will update like this => setTitle("Updated") then useState render again this component
+  const ClickHandler = () => {
+    setTitle("Updated!!");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={expenseitems.date} />
       <div className="expense-item__description">
-        <h2>{expenseitems.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${expenseitems.amount}</div>
+        <button onClick={ClickHandler}>Change Title</button>
       </div>
     </Card>
   );
